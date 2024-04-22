17b611392a360b21d274c4982933d7dce6802eb0
Oussama Ghazi <oussamaghazi.pf@gmail.com>
2024-04-22 14:39:46 +0200

    initial commit: authentication and anthorization with jwt

---
+++ src/main/java/com/agenceVoyage/backend/controller/AuthenticationController.java
@@ -0,0 +1,40 @@
+package com.agenceVoyage.backend.controller;
+
+import com.agenceVoyage.backend.service.AuthenticationService;
+import com.agenceVoyage.backend.model.AuthenticationResponse;
+import com.agenceVoyage.backend.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    private final AuthenticationService authService;
+
+    public AuthenticationController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody User request
+            ) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody User request
+    ) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @GetMapping("/login")
+    public ResponseEntity<String> LoginPage(){
+        return ResponseEntity.ok("please login in");
+    }
+}
